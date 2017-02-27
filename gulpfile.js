48391d2bd77036f0517743b0f523e4f64b50601a
Stefanos Dimitriadis <dimitriadis.stefan@gmail.com>
2017-02-27 20:10:15 +0200

    Gulp and PostCSS

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
+gulp.task('default', function() {
+	console.log('Hooray! You created a gulp task.');
+});
+
+gulp.task('html', function() {
+	console.log('Imagine something useful done to your HTML here.');
+});
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+	
+	watch('./app/index.html', function() {
+		gulp.start('html');
+	});
+	
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	});
+});
