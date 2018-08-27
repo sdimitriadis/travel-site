852b9e07ab6d83a4f67431c28964b73becb78705
Στέφανος Δημητριάδης <sdimitriadis@ots.gr>
2018-08-27 15:07:29 +0300

    Added browser support fallbacks.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,18 +2,31 @@ var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
 var del = require('del');
+var svg2png = require('gulp-svg2png');
 
 var config = {
-  mode: {
-      css: {
-          sprite: 'sprite.svg',
-          render: {
-              css: {
-                  template: './gulp/templates/sprite.css'
-              }
-          }
-      }
-  }  
+    shape: {
+        spacing: {
+            padding: 1
+        }
+    },
+    mode: {
+        css: {
+            variables: {
+                replaceSvgWithPng: function() {
+                    return function(sprite, render) {
+                        return render(sprite).split('.svg').join('.png');
+                    }
+                }
+            },
+            sprite: 'sprite.svg',
+            render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
+            }
+        }
+    }
 };
 
 gulp.task('beginClean', function() {
@@ -23,11 +36,17 @@ gulp.task('beginClean', function() {
 gulp.task('createSprite', ['beginClean'], function() {
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
-        .pipe(gulp.dest('./app/temp/sprite/'))
+        .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteGraphic', ['createSprite'], function() {
-    return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('createPngCopy', ['createSprite'], function() {
+    return gulp.src('./app/temp/sprite/css/*.svg')
+        .pipe(svg2png())
+        .pipe(gulp.dest('./app/temp/sprite/css'));
+});
+
+gulp.task('copySpriteGraphic', ['createPngCopy'], function() {
+    return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
         .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
@@ -41,4 +60,4 @@ gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
     return del('./app/temp/sprite');
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'createPngCopy', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
